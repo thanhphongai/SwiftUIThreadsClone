1cf0d859abfd3a43680587c12118736eca052e55
phongpt1 <phongpt1@imt-soft.com>
2024-05-03 20:41:22 +0700

    Initial Commit

---
+++ ThreadsTutorials/ThreadsTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialsApp.swift
+//  ThreadsTutorials
+//
+//  Created by macbookpro on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
